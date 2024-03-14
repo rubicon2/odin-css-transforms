50344bd17f4624ebf2a7d39e8f9869993f6aa1b5
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2024-03-14 11:44:52 +0000

    Switch between scenes every 10 seconds

--- src/index.js
+++ src/index.js
@@ -1,4 +1,23 @@
 import './style.css';
 import createCube from './cube/cube';
+import createHouseScene from './house/house';
 
-document.body.appendChild(createCube());
+const cube = createCube();
+document.body.appendChild(cube);
+
+const houseScene = createHouseScene();
+houseScene.style.display = 'none';
+document.body.appendChild(houseScene);
+
+let showingCube = true;
+setInterval(() => {
+  if (showingCube) {
+    cube.style.display = 'none';
+    houseScene.style.display = 'flex';
+    showingCube = false;
+  } else {
+    houseScene.style.display = 'none';
+    cube.style.display = 'flex';
+    showingCube = true;
+  }
+}, 10000);
